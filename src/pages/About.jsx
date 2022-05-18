baa8573635516cf2419fc5b9f2f0b7f429484967
DeploymentsPractice <deploymentspractice@gmail.com>
2022-05-18 23:47:30 +0530

    done with routing

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div>lorem ipsum dolor sit amet, consectetur adip</div>;
+}
+
+export default About;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function NotFound() {
+  return (
+    <div className="hero">
+      <div className="text-center hero-content">404 page not found</div>
+    </div>
+  );
+}
+
+export default NotFound;
