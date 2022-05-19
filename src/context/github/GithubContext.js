645b9955753c5b2b9a8414466f90b8918b4754ae
DeploymentsPractice <deploymentspractice@gmail.com>
2022-05-19 17:52:28 +0530

    search functionality added

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,80 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+    repos: [],
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({ q: text });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+    const { items } = await response.json();
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
+
+  const getUser = async (login) => {
+    setLoading();
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
+
+  const getUserRepos = async (login) => {
+    setLoading();
+    const params = new URLSearchParams({
+      per_page: 10,
+      sort: "created",
+    });
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos`);
+    const data = await response.json();
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,
+        repos: state.repos,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
